565ae7fdc84e742f2c31386e6800f58fd6e671cb
maechabin <mitoppo@gmail.com>
2018-11-18 03:23:13 +0900

    CRUD実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,89 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+
+interface Item {
+  id: string;
+  name: string;
+  age: number;
+}
 
 @Component({
   selector: 'app-root',
-  template: `
-    <ul>
-      <li *ngFor="let item of (items | async)">
-        name: {{ item.name }} / age: {{ item.age }}
-      </li>
-    </ul>
-  `,
+  templateUrl: 'app.component.html',
 })
-export class AppComponent {
-  items: Observable<any[]>;
+export class AppComponent implements OnInit {
+  // items: Observable<any[]>;
+  itemCollection: AngularFirestoreCollection<Item>;
+  itemDoc: AngularFirestoreDocument<Item>;
+  items: Observable<Item[]>;
+  item: Observable<Item>;
 
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('items').valueChanges();
+  name = new FormControl('');
+  age = new FormControl('');
+  updatedName = new FormControl('');
+  updatedAge = new FormControl('');
+  updatedId: string;
+
+  isDisabledDelete = false;
+  isDisabledEdit = true;
+
+  constructor(private afs: AngularFirestore) {
+    this.itemCollection = afs.collection<Item>('items');
+    this.itemDoc = afs.doc<Item>('name/mary');
+  }
+
+  ngOnInit(): void {
+    this.items = this.itemCollection.valueChanges();
+    this.item = this.itemDoc.valueChanges();
+    this.items.subscribe(a => {
+      console.log(a);
+    });
+    this.item.subscribe(a => {
+      console.log(a);
+    });
+  }
+
+  addItem(): void {
+    const id = this.afs.createId();
+    const item: Item = {
+      id,
+      name: this.name.value,
+      age: this.age.value,
+    };
+    this.itemCollection.doc(id).set(item);
+    this.name.setValue('');
+    this.age.setValue('');
+  }
+
+  editItem(item: Item): void {
+    this.updatedName.setValue(item.name);
+    this.updatedAge.setValue(item.age);
+    this.updatedId = item.id;
+    this.isDisabledDelete = true;
+    this.isDisabledEdit = false;
+  }
+
+  updateItem(): void {
+    const item = {
+      id: this.updatedId,
+      name: this.updatedName.value,
+      age: this.updatedAge.value,
+    };
+    this.itemCollection.doc(this.updatedId).update(item);
+    this.updatedName.setValue('');
+    this.updatedAge.setValue('');
+    this.updatedId = null;
+    this.isDisabledDelete = false;
+    this.isDisabledEdit = true;
+  }
+
+  deleteItem(item: Item): void {
+    this.itemCollection.doc(item.id).delete();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
@@ -9,6 +10,7 @@ import { environment } from '../environments/environment';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(
       environment.firebase,
       'angularfire2-sample',
